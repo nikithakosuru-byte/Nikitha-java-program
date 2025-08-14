cd029c02879a41444f8f7cdac52b787ae62fbe56
Nikitha <nikithakosuru@gmail.com>
2025-08-14 11:33:24 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,9 @@
+class Loop{
+    public static void main(String[] args) {
+        int i=0;
+        while(i<5) {
+            System.out.println("Hello,krishna");
+            i+=1;
+        }
+    }
+}
