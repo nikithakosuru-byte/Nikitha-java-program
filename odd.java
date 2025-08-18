60f287c97ea25890d8d0667c5385672789e9dbe0
Nikitha <nikithakosuru@gmail.com>
2025-08-18 10:32:21 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,9 @@
+class Odd{
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1;i<=n+1;i++){
+            System.out.println(i*2-1);
+        }
+        System.out.println(" ");
+    }
+}
