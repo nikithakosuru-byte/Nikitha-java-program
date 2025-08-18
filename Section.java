5efc709aad02fce182818023343cc638bb024454
Nikitha <nikithakosuru@gmail.com>
2025-08-18 10:27:03 +0530

    Add files via upload

---
+++ Section.java
@@ -0,0 +1,9 @@
+class Section{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n+1;i++){
+            System.out.println(i);
+        }
+        System.out.println(" ");
+    }
+}
