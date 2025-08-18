e7c38e916da36eff13d2240dc926def3b78a0d5a
Nikitha <nikithakosuru@gmail.com>
2025-08-18 10:20:22 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,9 @@
+class Even{
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1;i<=n+1;i++){
+            System.out.println(i*2);
+        }
+        System.out.println(" ");
+    }
+}
