ff1011003cb5711d88738f183bcf6f4f6c833398
Nikitha <nikithakosuru@gmail.com>
2025-08-19 14:02:42 +0530

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,40 @@
+class LinkedList {
+    Node head; 
+
+    class Node {
+        int data;
+        Node next;
+        Node(int d) { data = d; next = null; }
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null)
+            temp = temp.next;
+        temp.next = newNode;
+    }
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+
+        System.out.println("Linked List:");
+        list.printList();
+    }
+}
